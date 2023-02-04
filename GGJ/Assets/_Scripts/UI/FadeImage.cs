3d36f416c267222d65463de979a372246d7d4b67
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-02-04 11:11:23 +0100

    Added prefabs and scene management components

---
+++ GGJ/Assets/_Scripts/UI/FadeImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeImage : Singleton<FadeImage>
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        enabled = true;
+    }
+    
+    public void FadeAndLoad(string sceneName)
+    {
+        LeanTween.value(gameObject, f =>
+        {
+            canvasGroup.alpha = f;
+        }, 0, 1, .5f).setOnComplete((() =>
+        {
+            SceneManager.LoadSceneAsync(sceneName).completed += asyncOperation =>
+            {
+                LeanTween.value(gameObject, f =>
+                {
+                    canvasGroup.alpha= f;
+                }, 1, 0 , .5f);
+            };
+        }));
+    }
+}
+
---
+++ GGJ/Assets/_Scripts/UI/TransitionComponent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class TransitionComponent : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (sceneName == null) return;
+            FadeImage.Instance.FadeAndLoad(sceneName);
+        }
+    }
+}
+
