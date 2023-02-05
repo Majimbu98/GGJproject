eade9d864c6c89c866f96b07c9ac5830ab2b8ccc
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-02-05 13:10:12 +0100

    Added entity activator

---
+++ GGJ/Assets/_Scripts/AI/Entity/EntityActivator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityActivator : MonoBehaviour
+{
+    [SerializeField] private List<Entity> entitiesToActivate;
+    private Collectable owner;
+
+    private void Awake()
+    {
+        owner = GetComponent<Collectable>();
+    }
+
+    private void Start()
+    {
+        Inventory.Instance.OnItemPickup.AddListener(ActivateEntities);
+    }
+
+    private void ActivateEntities(InventoryEntry entry)
+    {
+        if(owner.entry != entry) return;
+
+        foreach (var entity in entitiesToActivate)
+        {
+            entity.gameObject.SetActive(true);
+            entity.agent.enabled = true;
+        }
+    }
+}
+
