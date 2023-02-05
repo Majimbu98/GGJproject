4caf9af3f06353e1b474d89e42c920b650572538
SigmyAlb <siegmayerofalberobello@gmail.com>
2023-02-05 01:52:58 +0100

    Updated WoodenLog
    Log now goes back to position instead of getting destroyed

--- GGJ/Assets/_Scripts/WoodenLog.cs
+++ GGJ/Assets/_Scripts/WoodenLog.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class WoodenLog : MonoBehaviour
 {
-    [SerializeField] public float durata = 10;
+    [SerializeField] Vector3 origine = new Vector3(0f,0f,0f);
     [SerializeField] Vector3 destinazione = new Vector3(0f,0f,10f);
+    [SerializeField] public float durata = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.move(this.gameObject, destinazione, durata);
-        StartCoroutine(RemoveLog());
+        StartCoroutine(MoveLog());
     }
     
-    IEnumerator RemoveLog(){
-        yield return new WaitForSeconds(durata);
-        Destroy(this.gameObject); 
+    IEnumerator MoveLog(){
+        while(true){
+            LeanTween.move(this.gameObject, destinazione, durata);
+            yield return new WaitForSeconds(durata);
+            transform.position = origine;
+        }
     }
 }
